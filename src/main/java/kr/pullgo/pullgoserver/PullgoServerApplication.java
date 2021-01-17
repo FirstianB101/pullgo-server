f4c11ea4dd7350965c52e8ea526a96447a46cb15
tellang <tellang@daum.net>
2021-01-17 15:56:05 +0900

    First commit

---
+++ src/main/java/kr/pullgo/pullgoserver/PullgoServerApplication.java
@@ -0,0 +1,13 @@
+package kr.pullgo.pullgoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullgoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullgoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pullgo/pullgoserver/PullgoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pullgo.pullgoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullgoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
