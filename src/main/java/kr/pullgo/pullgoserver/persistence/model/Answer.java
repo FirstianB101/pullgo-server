7c29def07e023e8a1e72d45e7abafaa9a5e3d516
woodyn1002 <woodyn1002@gmail.com>
2021-01-29 15:37:45 +0900

    Answer 클래스에 생성자 추가

--- src/main/java/kr/pullgo/pullgoserver/persistence/model/Answer.java
+++ src/main/java/kr/pullgo/pullgoserver/persistence/model/Answer.java
@@ -3,9 +3,21 @@ package kr.pullgo.pullgoserver.persistence.model;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 public class Answer {
 
     private Set<Integer> objectiveNumbers = new HashSet<>();
+
+    public Answer(Set<Integer> objectiveNumbers) {
+        this.objectiveNumbers = objectiveNumbers;
+    }
+
+    public Answer(int... numbers) {
+        for (int number : numbers) {
+            objectiveNumbers.add(number);
+        }
+    }
 }
