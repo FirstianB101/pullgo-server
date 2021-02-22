b6bb43eb948259f998bf6e21a69bef10d544d640
woodyn1002 <woodyn1002@gmail.com>
2021-02-22 22:09:41 +0900

    BaseRepository 클래스로 removeById() 중복 코드 최소화

--- src/main/java/kr/pullgo/pullgoserver/persistence/repository/QuestionRepository.java
+++ src/main/java/kr/pullgo/pullgoserver/persistence/repository/QuestionRepository.java
@@ -1,13 +1,9 @@
 package kr.pullgo.pullgoserver.persistence.repository;
 
 import kr.pullgo.pullgoserver.persistence.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends BaseRepository<Question, Long> {
 
-    @Transactional
-    int removeById(Long id);
 }
--- src/main/java/kr/pullgo/pullgoserver/persistence/repository/StudentRepository.java
+++ src/main/java/kr/pullgo/pullgoserver/persistence/repository/StudentRepository.java
@@ -1,13 +1,9 @@
 package kr.pullgo.pullgoserver.persistence.repository;
 
 import kr.pullgo.pullgoserver.persistence.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends BaseRepository<Student, Long> {
 
-    @Transactional
-    int removeById(Long id);
 }
