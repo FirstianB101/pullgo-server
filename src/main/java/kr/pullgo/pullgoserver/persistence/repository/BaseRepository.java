3b80561f421337aa5717912e4e074cda674af20b
woodyn1002 <woodyn1002@gmail.com>
2021-03-04 16:25:29 +0900

    Specification을 통해 리소스 목록을 조회하는 메소드 구현

--- src/main/java/kr/pullgo/pullgoserver/persistence/repository/BaseRepository.java
+++ src/main/java/kr/pullgo/pullgoserver/persistence/repository/BaseRepository.java
@@ -1,11 +1,12 @@
 package kr.pullgo.pullgoserver.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @NoRepositoryBean
-public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
     @Transactional
     int removeById(ID id);
