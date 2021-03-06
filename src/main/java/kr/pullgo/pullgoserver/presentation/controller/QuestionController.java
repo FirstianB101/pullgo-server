4839ff914c345343e3474ef6e160c645f7788a2f
woodyn1002 <woodyn1002@gmail.com>
2021-03-06 23:34:50 +0900

    Question 목록 조회 API에 쿼리 패러미터 기능 추가

--- src/main/java/kr/pullgo/pullgoserver/presentation/controller/QuestionController.java
+++ src/main/java/kr/pullgo/pullgoserver/presentation/controller/QuestionController.java
@@ -2,8 +2,11 @@ package kr.pullgo.pullgoserver.presentation.controller;
 
 import java.util.List;
 import kr.pullgo.pullgoserver.dto.QuestionDto;
+import kr.pullgo.pullgoserver.persistence.model.Question;
 import kr.pullgo.pullgoserver.service.QuestionService;
+import kr.pullgo.pullgoserver.service.spec.QuestionSpecs;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +35,15 @@ public class QuestionController {
     }
 
     @GetMapping("/exam/questions")
-    public List<QuestionDto.Result> list() {
-        return questionService.search();
+    public List<QuestionDto.Result> search(
+        @RequestParam(required = false) Long examId
+    ) {
+        Specification<Question> spec = null;
+        if (examId != null) {
+            spec = QuestionSpecs.belongsTo(examId).and(spec);
+        }
+
+        return questionService.search(spec);
     }
 
     @GetMapping("/exam/questions/{id}")
