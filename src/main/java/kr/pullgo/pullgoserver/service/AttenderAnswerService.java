1c124561e98e52a8487df6d520dbcbd23b2489a2
tellang <tellang@daum.net>
2021-03-09 14:10:16 +0900

    AttenderAnswer에서 AttenderState, Question과 연결추가

--- src/main/java/kr/pullgo/pullgoserver/service/AttenderAnswerService.java
+++ src/main/java/kr/pullgo/pullgoserver/service/AttenderAnswerService.java
@@ -4,8 +4,12 @@ import kr.pullgo.pullgoserver.dto.AttenderAnswerDto;
 import kr.pullgo.pullgoserver.dto.mapper.AttenderAnswerDtoMapper;
 import kr.pullgo.pullgoserver.persistence.model.Answer;
 import kr.pullgo.pullgoserver.persistence.model.AttenderAnswer;
+import kr.pullgo.pullgoserver.persistence.model.AttenderState;
+import kr.pullgo.pullgoserver.persistence.model.Question;
 import kr.pullgo.pullgoserver.persistence.repository.AttenderAnswerRepository;
+import kr.pullgo.pullgoserver.persistence.repository.AttenderStateRepository;
 import kr.pullgo.pullgoserver.persistence.repository.QuestionRepository;
+import kr.pullgo.pullgoserver.util.ResponseStatusExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,20 +21,30 @@ public class AttenderAnswerService extends
     private final AttenderAnswerDtoMapper dtoMapper;
     private final AttenderAnswerRepository attenderAnswerRepository;
     private final QuestionRepository questionRepository;
+    private final AttenderStateRepository attenderStateRepository;
 
     @Autowired
     public AttenderAnswerService(AttenderAnswerDtoMapper dtoMapper,
         AttenderAnswerRepository attenderAnswerRepository,
-        QuestionRepository questionRepository) {
+        QuestionRepository questionRepository,
+        AttenderStateRepository attenderStateRepository) {
         super(AttenderAnswer.class, dtoMapper, attenderAnswerRepository);
         this.dtoMapper = dtoMapper;
         this.attenderAnswerRepository = attenderAnswerRepository;
         this.questionRepository = questionRepository;
+        this.attenderStateRepository = attenderStateRepository;
     }
 
     @Override
     AttenderAnswer createOnDB(AttenderAnswerDto.Create dto) {
-        return attenderAnswerRepository.save(dtoMapper.asEntity(dto));
+        AttenderAnswer attenderAnswer = dtoMapper.asEntity(dto);
+        AttenderState attenderState = attenderStateRepository.findById(dto.getAttenderStateId())
+            .orElseThrow(ResponseStatusExceptions::attenderStateNotFound);
+        Question question = questionRepository.findById(dto.getQuestionId())
+            .orElseThrow(ResponseStatusExceptions::questionNotFound);
+        attenderState.addAnswer(attenderAnswer);
+        attenderAnswer.setQuestion(question);
+        return attenderAnswerRepository.save(attenderAnswer);
     }
 
     @Override
