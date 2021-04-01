45a2c14d17658aed2afa8ce9486e8b8cd0f82f93
woodyn1002 <woodyn1002@gmail.com>
2021-04-01 17:17:10 +0900

    수업 목록 조회 API에 날짜 기준으로 조회하는 쿼리 패러미터 추가 (#89)
    `sinceDate`와 `untilDate` 인자로 구현함

--- src/main/java/kr/pullgo/pullgoserver/service/spec/LessonSpecs.java
+++ src/main/java/kr/pullgo/pullgoserver/service/spec/LessonSpecs.java
@@ -1,8 +1,10 @@
 package kr.pullgo.pullgoserver.service.spec;
 
+import java.time.LocalDate;
 import javax.persistence.criteria.Join;
 import kr.pullgo.pullgoserver.persistence.model.Classroom;
 import kr.pullgo.pullgoserver.persistence.model.Lesson;
+import kr.pullgo.pullgoserver.persistence.model.Schedule;
 import kr.pullgo.pullgoserver.persistence.model.Student;
 import kr.pullgo.pullgoserver.persistence.model.Teacher;
 import org.springframework.data.jpa.domain.Specification;
@@ -29,6 +31,20 @@ public class LessonSpecs {
             Join<Lesson, Classroom> classroom = root.join("classroom");
             Join<Classroom, Teacher> teachers = classroom.joinSet("teachers");
             return builder.equal(teachers.get("id"), teacherId);
+        };
+    }
+
+    public static Specification<Lesson> sinceDate(LocalDate begin) {
+        return (root, query, builder) -> {
+            Join<Lesson, Schedule> schedule = root.join("schedule");
+            return builder.greaterThanOrEqualTo(schedule.get("date"), begin);
+        };
+    }
+
+    public static Specification<Lesson> untilDate(LocalDate endExclusive) {
+        return (root, query, builder) -> {
+            Join<Lesson, Schedule> schedule = root.join("schedule");
+            return builder.lessThan(schedule.get("date"), endExclusive);
         };
     }
 
