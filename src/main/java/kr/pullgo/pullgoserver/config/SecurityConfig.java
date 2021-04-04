37490681944e86173a92c5c09d2c7c1447c28289
woodyn1002 <woodyn1002@gmail.com>
2021-04-04 22:35:43 +0900

    비밀번호 저장 시 암호화 적용
    암호화 방식은 BCrypt 해시 함수를 사용함
    * 보안에 취약한 일반 해시 함수보다는, `key derivation function`을 활용하는 편이 비밀번호 암호화에 적합함
    * 다른 함수에 비해 구현이 쉬운 편임 (추후 보안 강화가 필요하면 개선하도록 함)

--- src/main/java/kr/pullgo/pullgoserver/config/SecurityConfig.java
+++ src/main/java/kr/pullgo/pullgoserver/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package kr.pullgo.pullgoserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/kr/pullgo/pullgoserver/service/AccountService.java
+++ src/main/java/kr/pullgo/pullgoserver/service/AccountService.java
@@ -5,20 +5,26 @@ import kr.pullgo.pullgoserver.dto.mapper.AccountDtoMapper;
 import kr.pullgo.pullgoserver.persistence.model.Account;
 import kr.pullgo.pullgoserver.persistence.model.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
     private final AccountDtoMapper dtoMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AccountService(AccountDtoMapper dtoMapper) {
+    public AccountService(AccountDtoMapper dtoMapper, PasswordEncoder passwordEncoder) {
         this.dtoMapper = dtoMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Account create(AccountDto.Create dto) {
         Account entity = dtoMapper.asEntity(dto);
+
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+        entity.setPassword(encodedPassword);
 
         entity.setRole(UserRole.USER);
 
@@ -27,7 +33,8 @@ public class AccountService {
 
     public void update(Account entity, AccountDto.Update dto) {
         if (dto.getPassword() != null) {
-            entity.setPassword(dto.getPassword());
+            String encodedPassword = passwordEncoder.encode(dto.getPassword());
+            entity.setPassword(encodedPassword);
         }
         if (dto.getFullName() != null) {
             entity.setFullName(dto.getFullName());
