f9f5381f4cc7d70882924073d3d240e4fca6e6ca
woodyn1002 <woodyn1002@gmail.com>
2021-05-23 16:20:24 +0900

    Spring Security 인증 필터 추가
    * `SecurityConfig`에 인증 관련 설정 추가
      * `BearerTokenAuthenticationFilter` Security Filter 추가
        * DispatcherServlet으로 요청이 전송될 때, Spring Security의 Filter가 미리 정의된 여러 Security Filter들을 실행하면서 사용자의 인증 정보를 추출함
        * 이때 추가적으로 적용된 `BearerTokenAuthenticationFilter`에서, `Authorization` 헤더로부터 JWT 문자열을 추출함 (헤더 내용은 `Authorization: Bearer TOKEN_HERE` 형식)
      * `JwtAuthenticationProvider` Authentication Provider 추가
        * Spring Security는 추출했던 사용자 인증 정보들을 토대로, 여러 Authentication Provider들을 통해 Security Context에 인증 정보를 설정함
        * 이때 추가적으로 적용된 `JwtAuthenticationProvider`에서, 추출했던 JWT의 유효성을 검사하며, 동시에 payload를 추출하여 `UserPrincipal`을 역직렬화 해냄
        * `UserPrincipal`을 Principal로 취급하는 `Authentication` 객체를 생성하고, 이를 Security Context에 설정함
        * 인증을 정상적으로 마치면, HTTP 요청이 처리되는 동안 `SecurityContextHolder.getContext().getAuthentication().getPrincipal()`을 통해, 사용자 신원이 담긴 `UserPrincipal` 인스턴스를 획득할 수 있음

--- src/main/java/kr/pullgo/pullgoserver/persistence/model/UserRole.java
+++ src/main/java/kr/pullgo/pullgoserver/persistence/model/UserRole.java
@@ -1,5 +1,12 @@
 package kr.pullgo.pullgoserver.persistence.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum UserRole {
-    USER, ADMIN
+    USER, ADMIN;
+
+    public GrantedAuthority asAuthority() {
+        return new SimpleGrantedAuthority(this.name());
+    }
 }
