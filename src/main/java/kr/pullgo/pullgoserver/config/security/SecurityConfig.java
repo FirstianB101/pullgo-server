0ced6a082443c1cb0559c134d5a8d7c7cf465693
woodyn1002 <woodyn1002@gmail.com>
2021-05-23 16:20:24 +0900

    Spring Security 인증 필터 추가
    * `SecurityConfig`에 인증 관련 설정 추가
      * `BearerTokenAuthenticationFilter` Security Filter 추가
        * DispatcherServlet으로 요청이 전송될 때, Spring Security의 Filter가 미리 정의된 여러 Security Filter들을 실행하면서 사용자의 인증 정보를 추출함
        * 이때 추가적으로 적용된 `BearerTokenAuthenticationFilter`에서, `Authorization` 헤더로부터 JWT 문자열을 추출함 (헤더 내용은 `Authorization: Bearer TOKEN_HERE` 형식)
      * `JwtAuthenticationProvider` Authentication Provider 추가
        * Spring Security는 추출했던 사용자 인증 정보들을 토대로, 여러 Authentication Provider들을 통해 Security Context에 인증 정보를 설정함
        * 이때 추가적으로 적용된 `JwtAuthenticationProvider`에서, 추출했던 JWT의 유효성을 검사하며, 동시에 payload를 추출하여 `UserPrincipal`을 역직렬화 해냄
        * `UserPrincipal`을 Principal로 취급하는 `Authentication` 객체를 생성하고, 이를 Security Context에 설정함
        * 인증을 정상적으로 마치면, HTTP 요청이 처리되는 동안 `SecurityContextHolder.getContext().getAuthentication().getPrincipal()`을 통해, 사용자 신원이 담긴 `UserPrincipal` 인스턴스를 획득할 수 있음

--- src/main/java/kr/pullgo/pullgoserver/config/security/SecurityConfig.java
+++ src/main/java/kr/pullgo/pullgoserver/config/security/SecurityConfig.java
@@ -1,25 +1,49 @@
 package kr.pullgo.pullgoserver.config.security;
 
+import kr.pullgo.pullgoserver.persistence.repository.AccountRepository;
+import kr.pullgo.pullgoserver.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtService jwtService;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public SecurityConfig(JwtService jwtService,
+        AccountRepository accountRepository) {
+        this.jwtService = jwtService;
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(new JwtAuthenticationProvider(jwtService, accountRepository));
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable();
+            .csrf().disable()
+            .addFilterBefore(new BearerTokenAuthenticationFilter(),
+                BasicAuthenticationFilter.class)
+            .authorizeRequests().anyRequest().permitAll();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
