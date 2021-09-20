db99ac7c202f22af8aa41ec37573151912e0d146
tellang <tellang@daum.net>
2021-09-21 01:25:07 +0900

    Aws codedeploy가 잘 작동하지 않은 문제 해결

--- after-install.sh
+++ after-install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd "deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/" || exit
 echo "after-install: pwd=$(pwd)"
 cp pullgo-server-0.0.1-SNAPSHOT.jar /home/ec2-user/pullgo-server.jar
 bash /home/ec2-user/deploy.sh
