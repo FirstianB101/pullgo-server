0c4b9feca132871a46d33ae3e5b60b2bdab8ea2b
tellang <tellang@daum.net>
2021-09-26 21:28:36 +0900

    local h2 console이 보이지 않은 문제 해결

--- src/main/java/kr/pullgo/pullgoserver/config/security/SecurityConfig.java
+++ src/main/java/kr/pullgo/pullgoserver/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtService = jwtService;
         this.accountRepository = accountRepository;
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
