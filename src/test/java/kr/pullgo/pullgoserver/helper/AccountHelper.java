2b7bb91ca9780ddbfa60bee96254d14f751f990f
tellang <tellang@daum.net>
2021-10-08 23:16:55 +0900

    AccountHelper의 기본생성 username을 UUID의 앞 8자리를 이용한 무작위 값을 부여한 username으로 변경

--- src/test/java/kr/pullgo/pullgoserver/helper/AccountHelper.java
+++ src/test/java/kr/pullgo/pullgoserver/helper/AccountHelper.java
@@ -1,19 +1,21 @@
 package kr.pullgo.pullgoserver.helper;
 
+import java.util.UUID;
 import kr.pullgo.pullgoserver.dto.AccountDto;
 import kr.pullgo.pullgoserver.persistence.model.Account;
 import kr.pullgo.pullgoserver.persistence.model.UserRole;
 
 public class AccountHelper {
 
-    private static final String ARBITRARY_USERNAME = "woodyn1002";
+    private static final String ARBITRARY_USERNAME = "user-";
     private static final String ARBITRARY_PASSWORD = "this!sPassw0rd";
     private static final String ARBITRARY_FULL_NAME = "최우진";
     private static final String ARBITRARY_PHONE = "01012345678";
 
     public static Account anAccount() {
+        String uuid = UUID.randomUUID().toString().substring(8);
         Account account = Account.builder()
-            .username(ARBITRARY_USERNAME)
+            .username(ARBITRARY_USERNAME+uuid)
             .password(ARBITRARY_PASSWORD)
             .fullName(ARBITRARY_FULL_NAME)
             .phone(ARBITRARY_PHONE)
@@ -24,8 +26,9 @@ public class AccountHelper {
     }
 
     public static AccountDto.Create anAccountCreateDto() {
+        String uuid = UUID.randomUUID().toString().substring(8);
         return AccountDto.Create.builder()
-            .username(ARBITRARY_USERNAME)
+            .username(ARBITRARY_USERNAME+uuid)
             .password(ARBITRARY_PASSWORD)
             .fullName(ARBITRARY_FULL_NAME)
             .phone(ARBITRARY_PHONE)
@@ -41,8 +44,9 @@ public class AccountHelper {
     }
 
     public static AccountDto.Result anAccountResultDto() {
+        String uuid = UUID.randomUUID().toString().substring(8);
         return AccountDto.Result.builder()
-            .username(ARBITRARY_USERNAME)
+            .username(ARBITRARY_USERNAME+uuid)
             .fullName(ARBITRARY_FULL_NAME)
             .phone(ARBITRARY_PHONE)
             .role(UserRole.USER)
