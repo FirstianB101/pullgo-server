4eb6bf63cabd94f9ee4321a99c35768b4dc3f16c
tellang <tellang@daum.net>
2021-10-12 16:06:53 +0900

    EntityHelper에 토큰생성 기능 추가에 따른 테스트상의 의존성 추가

--- src/test/java/kr/pullgo/pullgoserver/persistence/model/QuestionTest.java
+++ src/test/java/kr/pullgo/pullgoserver/persistence/model/QuestionTest.java
@@ -2,16 +2,18 @@ package kr.pullgo.pullgoserver.persistence.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import kr.pullgo.pullgoserver.helper.EntityHelper;
+import kr.pullgo.pullgoserver.service.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
-@Import(EntityHelper.class)
+@Import({EntityHelper.class, JwtService.class, ObjectMapper.class})
 public class QuestionTest {
 
     @Autowired
