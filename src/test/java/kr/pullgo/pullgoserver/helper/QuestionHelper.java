82cb95bd6b88db68b703f31c444dedf14bb7511f
tellang <tellang@daum.net>
2021-10-16 17:13:44 +0900

    Question 엔티티 필드 변경에 따른 QuestionHelper builder 내용 수정

--- src/test/java/kr/pullgo/pullgoserver/helper/QuestionHelper.java
+++ src/test/java/kr/pullgo/pullgoserver/helper/QuestionHelper.java
@@ -2,9 +2,11 @@ package kr.pullgo.pullgoserver.helper;
 
 import static kr.pullgo.pullgoserver.helper.ExamHelper.anExam;
 
+import java.util.Map;
 import java.util.Set;
 import kr.pullgo.pullgoserver.dto.QuestionDto;
 import kr.pullgo.pullgoserver.persistence.model.Answer;
+import kr.pullgo.pullgoserver.persistence.model.Choice;
 import kr.pullgo.pullgoserver.persistence.model.Question;
 
 public class QuestionHelper {
@@ -15,11 +17,15 @@ public class QuestionHelper {
         "https://avatars.githubusercontent.com/u/77564110?s=200&v=4";
     private static final Set<Integer> ARBITRARY_ANSWER = Set.of(1);
 
+    private static final Map<String ,String> ARBITRARY_CHOICES = Map.of(
+        "1", "1", "2", "2", "3", "3", "4", "4", "5", "5");
+
     public static Question aQuestion() {
         Question question = Question.builder()
             .content(ARBITRARY_CONTENT)
             .pictureUrl(ARBITRARY_PICTURE_URL)
             .answer(new Answer(ARBITRARY_ANSWER))
+            .choice(new Choice(ARBITRARY_CHOICES))
             .build();
         question.setId(0L);
         question.setExam(anExam());
@@ -32,6 +38,7 @@ public class QuestionHelper {
             .content(ARBITRARY_CONTENT)
             .pictureUrl(ARBITRARY_PICTURE_URL)
             .answer(ARBITRARY_ANSWER)
+            .choice(ARBITRARY_CHOICES)
             .build();
     }
 
@@ -40,6 +47,7 @@ public class QuestionHelper {
             .content(ARBITRARY_CONTENT)
             .pictureUrl(ARBITRARY_PICTURE_URL)
             .answer(ARBITRARY_ANSWER)
+            .choice(ARBITRARY_CHOICES)
             .build();
     }
 
@@ -50,6 +58,7 @@ public class QuestionHelper {
             .content(ARBITRARY_CONTENT)
             .pictureUrl(ARBITRARY_PICTURE_URL)
             .answer(ARBITRARY_ANSWER)
+            .choice(ARBITRARY_CHOICES)
             .build();
     }
 
