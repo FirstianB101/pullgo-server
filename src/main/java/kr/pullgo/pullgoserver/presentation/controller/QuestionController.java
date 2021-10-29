a07d61c4983de6e81d0fffd4daaf4c3f65118be9
tellang <tellang@daum.net>
2021-10-29 20:05:56 +0900

    클라이언트의 요구에따라 1개의 question을 만들 수 있었던 코드를 1개이상의 question을 한번의 post요청으로 다중생성할 수 있도록 변경

--- src/main/java/kr/pullgo/pullgoserver/presentation/controller/QuestionController.java
+++ src/main/java/kr/pullgo/pullgoserver/presentation/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package kr.pullgo.pullgoserver.presentation.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 import kr.pullgo.pullgoserver.dto.QuestionDto;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class QuestionController {
@@ -32,10 +35,31 @@ public class QuestionController {
     }
 
     @PostMapping("/exam/questions")
-    @ResponseStatus(HttpStatus.CREATED)
-    public QuestionDto.Result post(@Valid @RequestBody QuestionDto.Create dto,
-        Authentication authentication) {
-        return questionService.create(dto, authentication);
+    public ResponseEntity<List<ResponseEntity<QuestionDto.Result>>> post(
+        Authentication authentication,
+        @Valid @RequestBody QuestionDto.MultipleCreate dto) {
+        boolean isError = false;
+        List<ResponseEntity<QuestionDto.Result>> results = new ArrayList<>();
+        for (var configDto : dto.getQuestionConfigs()) {
+            try {
+                results.add(new ResponseEntity<>(
+                    questionService.create(QuestionDto.Create.builder()
+                        .examId(dto.getExamId())
+                        .questionConfig(configDto).
+                        build(), authentication),
+                    HttpStatus.CREATED));
+            } catch (ResponseStatusException e) {
+                results.add(new ResponseEntity<>(QuestionDto.Result.builder()
+                    .examId(dto.getExamId())
+                    .questionConfig(configDto).
+                    build(), e.getStatus()));
+                isError = true;
+            }
+        }
+        if (isError)
+            return new ResponseEntity<>(results, HttpStatus.BAD_REQUEST);
+        else
+            return new ResponseEntity<>(results, HttpStatus.OK);
     }
 
     @GetMapping("/exam/questions")
