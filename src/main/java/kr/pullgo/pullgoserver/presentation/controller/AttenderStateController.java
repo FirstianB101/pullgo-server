c6384455ae8dfebc35ca73305bd647306cc4d4be
tellang <tellang@daum.net>
2021-11-05 17:26:12 +0900

    누락된 Validation의존성 추가

--- src/main/java/kr/pullgo/pullgoserver/presentation/controller/AttenderStateController.java
+++ src/main/java/kr/pullgo/pullgoserver/presentation/controller/AttenderStateController.java
@@ -11,9 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 public class AttenderStateController {
 
