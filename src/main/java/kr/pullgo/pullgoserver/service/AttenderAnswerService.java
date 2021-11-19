0dfa74f9133ef876cdc4f4c909ca68fc851a53d0
tellang <tellang@daum.net>
2021-11-19 21:57:09 +0900

    AttenderAnswerService에 검증 조건 추가
    유효하지 않은 AttenderState, Exam의 상태에서 AttenderAnswer를 수정할 수 없도록 검증 조건 추가

--- src/main/java/kr/pullgo/pullgoserver/service/AttenderAnswerService.java
+++ src/main/java/kr/pullgo/pullgoserver/service/AttenderAnswerService.java
@@ -1,15 +1,19 @@
 package kr.pullgo.pullgoserver.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import kr.pullgo.pullgoserver.dto.AttenderAnswerDto;
 import kr.pullgo.pullgoserver.dto.mapper.AttenderAnswerDtoMapper;
 import kr.pullgo.pullgoserver.persistence.model.Answer;
 import kr.pullgo.pullgoserver.persistence.model.AttenderAnswer;
 import kr.pullgo.pullgoserver.persistence.model.AttenderState;
+import kr.pullgo.pullgoserver.persistence.model.AttendingProgress;
+import kr.pullgo.pullgoserver.persistence.model.Exam;
 import kr.pullgo.pullgoserver.persistence.model.Question;
 import kr.pullgo.pullgoserver.persistence.repository.AttenderAnswerRepository;
 import kr.pullgo.pullgoserver.service.authorizer.AttenderAnswerAuthorizer;
 import kr.pullgo.pullgoserver.service.helper.RepositoryHelper;
+import kr.pullgo.pullgoserver.service.helper.ServiceErrorHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,16 +29,19 @@ public class AttenderAnswerService {
     private final AttenderAnswerDtoMapper dtoMapper;
     private final AttenderAnswerRepository attenderAnswerRepository;
     private final RepositoryHelper repoHelper;
+    private final ServiceErrorHelper errorHelper;
     private final AttenderAnswerAuthorizer attenderAnswerAuthorizer;
 
     @Autowired
     public AttenderAnswerService(AttenderAnswerDtoMapper dtoMapper,
         AttenderAnswerRepository attenderAnswerRepository,
         RepositoryHelper repoHelper,
+        ServiceErrorHelper errorHelper,
         AttenderAnswerAuthorizer attenderAnswerAuthorizer) {
         this.dtoMapper = dtoMapper;
         this.attenderAnswerRepository = attenderAnswerRepository;
         this.repoHelper = repoHelper;
+        this.errorHelper = errorHelper;
         this.attenderAnswerAuthorizer = attenderAnswerAuthorizer;
     }
 
@@ -43,6 +50,7 @@ public class AttenderAnswerService {
         Authentication authentication) {
         AttenderState attenderState = repoHelper.findAttenderStateOrThrow(dto.getAttenderStateId());
         attenderAnswerAuthorizer.requireByOneself(authentication, attenderState.getAttender());
+        checkAttenderStateValidations(attenderState);
 
         AttenderAnswer attenderAnswer = dtoMapper.asEntity(dto);
 
@@ -74,6 +82,9 @@ public class AttenderAnswerService {
         AttenderAnswer entity = repoHelper.findAttenderAnswerOrThrow(attenderStateId, questionId);
         attenderAnswerAuthorizer.requireOwningAttender(authentication, entity);
 
+        AttenderState attenderState = entity.getAttenderState();
+        checkAttenderStateValidations(attenderState);
+
         if (dto.getAnswer() != null) {
             entity.setAnswer(new Answer(dto.getAnswer()));
         }
@@ -93,5 +104,27 @@ public class AttenderAnswerService {
         throws ResponseStatusException {
         return repoHelper.findAttenderStateOrThrow(attenderStateId)
             .getAnswers().stream().anyMatch(it -> it.getQuestion().getId().equals(questionId));
+    }
+
+    private void checkAttenderStateValidations(AttenderState attenderState) {
+        Exam exam = attenderState.getExam();
+
+        AttendingProgress presentAttendingProgress = attenderState.getProgress();
+
+        if (presentAttendingProgress != AttendingProgress.ONGOING) {
+            throw errorHelper.badRequest("Attender state is already " + presentAttendingProgress);
+        }
+        if (attenderState.isAfterTimeLimit(LocalDateTime.now())) {
+            throw errorHelper.badRequest("It's been updated beyond the timeout");
+        }
+        if (attenderState.isOutOfTimeRange(LocalDateTime.now())) {
+            throw errorHelper.badRequest("It's been updated after time range");
+        }
+        if (exam.isFinished()) {
+            throw errorHelper.badRequest("It's been updated already finished exam");
+        }
+        if (exam.isCancelled()) {
+            throw errorHelper.badRequest("It's been updated already cancelled exam");
+        }
     }
 }
