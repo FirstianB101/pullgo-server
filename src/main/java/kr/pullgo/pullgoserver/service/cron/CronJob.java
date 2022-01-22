f9822357077c5c0498b8fd9cc5a723d7317e49ec
tellang <tellang@daum.net>
2022-01-22 21:43:01 +0900

    Spring Scheduling을 이용한 예약작업 컨포넌트 추가

---
+++ src/main/java/kr/pullgo/pullgoserver/service/cron/CronJob.java
@@ -0,0 +1,41 @@
+package kr.pullgo.pullgoserver.service.cron;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@EnableScheduling
+@Component
+public class CronJob {
+
+    private final TaskScheduler scheduler;
+    private final Map<Long, ScheduledFuture<?>> scheduledTasks = new ConcurrentHashMap<>();
+
+
+    @Autowired
+    public CronJob(
+        TaskScheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    public void register(Long id, Runnable runnable, LocalDateTime endTime, String msg) {
+        ScheduledFuture<?> task = scheduler.schedule(runnable, Timestamp.valueOf(endTime));
+        scheduledTasks.put(id, task);
+        log.info("regiser " + msg + " timer, id: [" + id + "], until :" + endTime + '\n');
+    }
+
+    public void remove(Long id, String msg) {
+        scheduledTasks.get(id).cancel(true);
+        log.info("remove " + msg + " timer, id: [" + id + "]" + '\n');
+    }
+}
+
+
